3c881c9e16c06abea047ee088044cc391a5a95f6
Ricardo <ricardoslee@gmail.com>
2018-08-15 18:14:17 -0300

    tarefa GAEE

---
+++ GAEE/Tarefa 4/Untitled.R
@@ -0,0 +1,6 @@
+data(oldcol)
+col.W <- nb2listw(COL.nb, style="W")
+crime <- COL.OLD$CRIME
+str(moran(crime, col.W, length(COL.nb), Szero(col.W)))
+is.na(crime) <- sample(1:length(crime), 10)
+str(moran(crime, col.W, length(COL.nb), Szero(col.W), NAOK=TRUE))
